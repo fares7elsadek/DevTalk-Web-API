db60d59915e3e660f86b397534dd2ce398c36c39
fares elsadek <2021170383@cis.asu.edu.eg>
2025-03-04 17:37:05 +0200

    update samsite on refresh token

--- src/DevTalk.API/Controllers/AuthController.cs
+++ src/DevTalk.API/Controllers/AuthController.cs
@@ -172,6 +172,8 @@ namespace DevTalk.API.Controllers
             {
                 HttpOnly = true,
                 Expires = expires,
+                Secure = true,
+                SameSite = SameSiteMode.None
             };
             Response.Cookies.Append("refreshToken", refreshToken, cookieOptions);
         }
