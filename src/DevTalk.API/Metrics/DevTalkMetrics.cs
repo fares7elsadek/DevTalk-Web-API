93c592f26f86dbc4d77c8638e94e5d477f009d00
fares7elsadek <fares7elsadek@gmail.com>
2026-07-16 16:51:09 +0300

    Observability Setup

---
+++ src/DevTalk.API/Metrics/DevTalkMetrics.cs
@@ -0,0 +1,13 @@
+﻿using System.Diagnostics.Metrics;
+
+namespace DevTalk.API.Metrics;
+
+public static class DevTalkMetrics
+{
+    public static readonly Meter Meter = new("DevTalk", "1.0.0");
+
+    public static readonly Counter<long> RequestCounter =
+        Meter.CreateCounter<long>(
+            "devtalk.requests.count",
+            description: "Number of DevTalk requests");
+}
